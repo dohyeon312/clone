cd7498c3bddb7428eb22e0b521d720d264daaaf0
dohyeon <rlaehguscjswo1101@naver.com>
2024-03-28 20:33:28 +0900

    20240328
    Move

---
+++ 애로우/Assets/script/player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed ;
+    private bool isTouchLeft;
+    private bool isTouchRight;
+    // Update is called once per frame
+    void Update()
+    {
+        Move(); 
+    }
+    public void Move() 
+    {
+        // 왼쪽 화살표가 눌렸을 때
+        if (Input.GetKey(KeyCode.LeftArrow)&&!isTouchLeft)
+        {
+            transform.Translate(-speed, 0, 0); // 왼쪽으로 「3」 움직인다
+        }
+
+        // 오른쪽 화살표가 눌렸을 때
+        if (Input.GetKey(KeyCode.RightArrow)&&!isTouchRight)
+        {
+            transform.Translate(speed, 0, 0); // 오른쪽으로 「3」 움직인다
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border") 
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true; 
+                    break;
+
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+
+            }
+        }
+    }
+
+}
+
